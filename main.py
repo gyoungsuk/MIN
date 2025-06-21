d87be5f751fcb49fdaf043a264b56d5edef57478
gyoungsuk <mingyoungsuk@gmail.com>
2025-06-21 12:43:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-st title('skdml cjt streamlit 프로젝트!')
-st.write("kkkkk")
+st.markdown("🌟 **잘했어요!** 당신의 노력이 빛났어요! ✨")
+st.markdown("🐣 **똑똑이 등장!** 정말 똑똑하게 잘 해결했어요! 🧠")
+st.markdown("🍭 **열심히 했어요!** 한 걸음 한 걸음이 소중해요 💪")
+st.markdown("🎯 **정확해요!** 완벽한 해결이에요 💡")
+st.markdown("🌈 **반짝반짝 아이디어!** 참신하고 멋진 생각이에요 🌟")
